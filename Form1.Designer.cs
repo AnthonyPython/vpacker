c4097a92b6cf2551ae22b4baec4007158474d985
Nbc66 <hl2.for.you@gmail.com>
2021-09-23 21:31:57 +0200

    early ui work using MaterialSkin.2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,15 +31,15 @@ namespace Vpacker
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.BrowseGameDirectory = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxGameDirectory = new System.Windows.Forms.TextBox();
             this.folderBrowserDialogGame = new System.Windows.Forms.FolderBrowserDialog();
             this.tabVpack = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new MaterialSkin.Controls.MaterialButton();
+            this.comboBox_Mods = new MaterialSkin.Controls.MaterialComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.comboBox_Mods = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxExtraParams = new System.Windows.Forms.TextBox();
@@ -72,18 +72,6 @@ namespace Vpacker
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(227, 126);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 47);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Pack Files";
-            this.toolTip1.SetToolTip(this.button1, "Pack the mod folders/files into vpk\'s.");
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // BrowseGameDirectory
             // 
             this.BrowseGameDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -99,7 +87,7 @@ namespace Vpacker
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-3, 417);
+            this.label1.Location = new System.Drawing.Point(11, 464);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 25);
             this.label1.TabIndex = 3;
@@ -119,16 +107,17 @@ namespace Vpacker
             this.tabVpack.Controls.Add(this.tabPage3);
             this.tabVpack.Controls.Add(this.tabPage4);
             this.tabVpack.Controls.Add(this.tabPage2);
-            this.tabVpack.Location = new System.Drawing.Point(12, 12);
+            this.tabVpack.Location = new System.Drawing.Point(16, 77);
             this.tabVpack.Name = "tabVpack";
             this.tabVpack.SelectedIndex = 0;
-            this.tabVpack.Size = new System.Drawing.Size(611, 371);
+            this.tabVpack.Size = new System.Drawing.Size(611, 373);
             this.tabVpack.TabIndex = 5;
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.comboBox_Mods);
+            this.tabPage1.Controls.Add(this.label7);
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.textBoxExtraParams);
@@ -137,41 +126,74 @@ namespace Vpacker
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.textBoxCSize);
             this.tabPage1.Controls.Add(this.richTextBoxLog);
-            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(603, 345);
+            this.tabPage1.Size = new System.Drawing.Size(603, 347);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Main";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = false;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.button1.Depth = 0;
+            this.button1.HighEmphasis = true;
+            this.button1.Icon = null;
+            this.button1.Location = new System.Drawing.Point(198, 116);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.button1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 36);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Pack FIles";
+            this.toolTip1.SetToolTip(this.button1, "Pack the mod folders/files into vpk\'s.");
+            this.button1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.button1.UseAccentColor = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox_Mods
+            // 
+            this.comboBox_Mods.AutoResize = false;
+            this.comboBox_Mods.BackColor = System.Drawing.Color.White;
+            this.comboBox_Mods.Depth = 0;
+            this.comboBox_Mods.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.comboBox_Mods.DropDownHeight = 174;
+            this.comboBox_Mods.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Mods.DropDownWidth = 121;
+            this.comboBox_Mods.Font = new System.Drawing.Font("Roboto Medium", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.comboBox_Mods.ForeColor = System.Drawing.Color.White;
+            this.comboBox_Mods.FormattingEnabled = true;
+            this.comboBox_Mods.IntegralHeight = false;
+            this.comboBox_Mods.ItemHeight = 43;
+            this.comboBox_Mods.Location = new System.Drawing.Point(175, 58);
+            this.comboBox_Mods.MaxDropDownItems = 4;
+            this.comboBox_Mods.MouseState = MaterialSkin.MouseState.OUT;
+            this.comboBox_Mods.Name = "comboBox_Mods";
+            this.comboBox_Mods.Size = new System.Drawing.Size(234, 49);
+            this.comboBox_Mods.StartIndex = 0;
+            this.comboBox_Mods.TabIndex = 16;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(126, 69);
+            this.label7.Location = new System.Drawing.Point(122, 68);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(51, 25);
             this.label7.TabIndex = 15;
             this.label7.Text = "Mod";
             // 
-            // comboBox_Mods
-            // 
-            this.comboBox_Mods.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_Mods.FormattingEnabled = true;
-            this.comboBox_Mods.Location = new System.Drawing.Point(183, 69);
-            this.comboBox_Mods.Name = "comboBox_Mods";
-            this.comboBox_Mods.Size = new System.Drawing.Size(282, 21);
-            this.comboBox_Mods.TabIndex = 14;
-            this.toolTip1.SetToolTip(this.comboBox_Mods, "Choose a mod to pack files for.");
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(35, 194);
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(19, 173);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(451, 13);
+            this.label5.Size = new System.Drawing.Size(453, 15);
             this.label5.TabIndex = 13;
             this.label5.Text = "Warning: Do not change  these unless you know what you are doing! You have been w" +
     "arned.";
@@ -179,7 +201,7 @@ namespace Vpacker
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(393, 213);
+            this.label4.Location = new System.Drawing.Point(386, 203);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 13);
             this.label4.TabIndex = 12;
@@ -187,14 +209,14 @@ namespace Vpacker
             // 
             // textBoxExtraParams
             // 
-            this.textBoxExtraParams.Location = new System.Drawing.Point(311, 229);
+            this.textBoxExtraParams.Location = new System.Drawing.Point(299, 229);
             this.textBoxExtraParams.Name = "textBoxExtraParams";
             this.textBoxExtraParams.Size = new System.Drawing.Size(280, 20);
             this.textBoxExtraParams.TabIndex = 11;
             // 
             // textBoxCNBounds
             // 
-            this.textBoxCNBounds.Location = new System.Drawing.Point(203, 229);
+            this.textBoxCNBounds.Location = new System.Drawing.Point(215, 229);
             this.textBoxCNBounds.Name = "textBoxCNBounds";
             this.textBoxCNBounds.Size = new System.Drawing.Size(36, 20);
             this.textBoxCNBounds.TabIndex = 10;
@@ -203,7 +225,7 @@ namespace Vpacker
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(180, 213);
+            this.label3.Location = new System.Drawing.Point(180, 203);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 13);
             this.label3.TabIndex = 9;
@@ -213,7 +235,7 @@ namespace Vpacker
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 213);
+            this.label2.Location = new System.Drawing.Point(94, 203);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 13);
             this.label2.TabIndex = 8;
@@ -222,7 +244,7 @@ namespace Vpacker
             // 
             // textBoxCSize
             // 
-            this.textBoxCSize.Location = new System.Drawing.Point(119, 229);
+            this.textBoxCSize.Location = new System.Drawing.Point(106, 229);
             this.textBoxCSize.Name = "textBoxCSize";
             this.textBoxCSize.Size = new System.Drawing.Size(33, 20);
             this.textBoxCSize.TabIndex = 7;
@@ -230,9 +252,9 @@ namespace Vpacker
             // 
             // richTextBoxLog
             // 
-            this.richTextBoxLog.Location = new System.Drawing.Point(20, 268);
+            this.richTextBoxLog.Location = new System.Drawing.Point(38, 255);
             this.richTextBoxLog.Name = "richTextBoxLog";
-            this.richTextBoxLog.Size = new System.Drawing.Size(571, 77);
+            this.richTextBoxLog.Size = new System.Drawing.Size(541, 77);
             this.richTextBoxLog.TabIndex = 5;
             this.richTextBoxLog.Text = "";
             // 
@@ -245,7 +267,7 @@ namespace Vpacker
             this.tabPage3.Controls.Add(this.checkBoxMultichunk);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(603, 345);
+            this.tabPage3.Size = new System.Drawing.Size(603, 347);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Batch";
             this.tabPage3.ToolTipText = "pack single or multiple folders ";
@@ -307,7 +329,7 @@ namespace Vpacker
             this.tabPage4.Controls.Add(this.BrowseGameDirectory);
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(603, 345);
+            this.tabPage4.Size = new System.Drawing.Size(603, 347);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Settings";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -342,7 +364,7 @@ namespace Vpacker
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(603, 345);
+            this.tabPage2.Size = new System.Drawing.Size(603, 347);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Info";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -350,7 +372,7 @@ namespace Vpacker
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(11, 163);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 163);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(91, 92);
             this.pictureBox1.TabIndex = 5;
@@ -358,13 +380,12 @@ namespace Vpacker
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(6, 131);
+            this.label6.Location = new System.Drawing.Point(4, 101);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(292, 29);
+            this.label6.Size = new System.Drawing.Size(292, 59);
             this.label6.TabIndex = 4;
-            this.label6.Text = "Created by Anthonypython";
+            this.label6.Text = "Created by Anthonypython and Nbc66";
             // 
             // versionlabel
             // 
@@ -392,9 +413,9 @@ namespace Vpacker
             // button_Refresh
             // 
             this.button_Refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Refresh.Location = new System.Drawing.Point(454, 397);
+            this.button_Refresh.Location = new System.Drawing.Point(456, 450);
             this.button_Refresh.Name = "button_Refresh";
-            this.button_Refresh.Size = new System.Drawing.Size(143, 47);
+            this.button_Refresh.Size = new System.Drawing.Size(143, 36);
             this.button_Refresh.TabIndex = 16;
             this.button_Refresh.Text = "Refresh";
             this.toolTip1.SetToolTip(this.button_Refresh, "Refreshes steam library & Source Mod list");
@@ -405,7 +426,7 @@ namespace Vpacker
             // 
             this.comboBox_VpkGame.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_VpkGame.FormattingEnabled = true;
-            this.comboBox_VpkGame.Location = new System.Drawing.Point(114, 423);
+            this.comboBox_VpkGame.Location = new System.Drawing.Point(147, 456);
             this.comboBox_VpkGame.Name = "comboBox_VpkGame";
             this.comboBox_VpkGame.Size = new System.Drawing.Size(282, 21);
             this.comboBox_VpkGame.TabIndex = 17;
@@ -415,12 +436,16 @@ namespace Vpacker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.ClientSize = new System.Drawing.Size(640, 497);
             this.Controls.Add(this.button_Refresh);
             this.Controls.Add(this.comboBox_VpkGame);
             this.Controls.Add(this.tabVpack);
             this.Controls.Add(this.label1);
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_48;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3, 72, 3, 3);
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Vpacker";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabVpack.ResumeLayout(false);
@@ -439,8 +464,6 @@ namespace Vpacker
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button BrowseGameDirectory;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxGameDirectory;
@@ -463,7 +486,6 @@ namespace Vpacker
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox_Mods;
         private System.Windows.Forms.Button button_Refresh;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.RichTextBox richTextBox_Folders;
@@ -473,6 +495,8 @@ namespace Vpacker
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.CheckBox checkBox_manualvpkpath;
+        private MaterialSkin.Controls.MaterialComboBox comboBox_Mods;
+        private MaterialSkin.Controls.MaterialButton button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Win32;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using Microsoft.Win32;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,7 +17,7 @@ using System.Windows.Forms;
 
 namespace Vpacker
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public string vpk_dir = "";
 
@@ -38,6 +40,14 @@ namespace Vpacker
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+
         }
 
         public static Steam steam = new Steam();
